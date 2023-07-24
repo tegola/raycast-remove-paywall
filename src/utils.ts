82bad2b80828929df88f501644333853d4354404
Alan Sprecacenere <alan@qreate.it>
2023-07-24 12:35:18 +0200

    Add "remove paywall using" command

---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+import { Clipboard, getSelectedText } from "@raycast/api";
+
+export function isUrl(text: string): boolean {
+  return /^https?:\/\//.test(text);
+}
+
+export async function getExistingText(fallbackText = ""): Promise<string> {
+  let selectedText = "";
+  let clipboardText: string | undefined = "";
+  fallbackText = fallbackText?.trim() ?? "";
+
+  // Get the selected text (if any)
+  try {
+    selectedText = (await getSelectedText()).trim();
+  } catch {
+    // Ignore errors
+  }
+
+  // Get the clipboard text (if any)
+  try {
+    clipboardText = await Clipboard.readText();
+    clipboardText = clipboardText ? clipboardText.trim() : "";
+  } catch {
+    // Ignore errors
+  }
+
+  console.log("getExistingText", { selectedText, fallbackText, clipboardText });
+
+  return [selectedText, fallbackText, clipboardText].find((v) => !!v) ?? "";
+}
